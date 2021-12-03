76544f0db2062c3c8fb2ebf9dd9a340709306dcb
Matthias Biermans <matthias.biermans@databrewers.io>
2021-12-03 16:19:52 +0100

    Rewrite code

--- src/main/scala/databrewers/adventofcode/Day03.scala
+++ src/main/scala/databrewers/adventofcode/Day03.scala
@@ -6,54 +6,51 @@ import scala.collection.BitSet
 
 object Day03 extends App {
 
-  def getBitString(list: List[String]) = {
-    val pivoted = list match {
-      case head :: _ =>
-        for {
-          indexX <- head.indices
-        } yield list.foldLeft("") { case (accum, next) => accum.appended(next.charAt(indexX)) }
-      case Nil => Seq.empty[String]
-    }
-    pivoted
-      .map { case str =>
-        if (str.count(char => char == '1') >= str.count(char => char == '0')) "1" else "0"
-      }
-      .mkString("")
-  }
+  val One = '1'
+  val Zero = '0'
 
-  def flipBits: Char => Char = (c: Char) =>
+  def flipBit: Char => Char = (c: Char) =>
     c match {
       case '0' => '1'
       case '1' => '0'
     }
 
-  def filterBasedOnCharIndex(list: List[String], index: Int, charMap: Char => Char = identity): String = {
+  def leastCommon: List[Char] => Char = { list =>
+    if (list.count(_ == Zero) < list.count(_ == One))
+      Zero
+    else
+      One
+  }
 
-    def _filterBasedOnCharIndex(list: List[String], index: Int, comp: String, charMap: Char => Char): String = {
-      if (list.size == 1) {
-        list.head
-      } else {
-        val filteredList = list.filter(str => str(index) == comp(index))
-        val bitString = getBitString(filteredList).map(charMap)
-        _filterBasedOnCharIndex(filteredList, index + 1, bitString, charMap)
-      }
+  def mostCommon: List[Char] => Char = { list =>
+    if (list.count(_ == Zero) >= list.count(_ == One))
+      Zero
+    else
+      One
+  }
+
+  def findBitString(nums: List[List[Char]], index: Int = 0)(commonOf: List[Char] => Char): Int = {
+    if (nums.size == 1) {
+      Integer.parseInt(nums.head.mkString(""), 2)
+    } else {
+
+      val common: Char = commonOf(nums.transpose.apply(index))
+      val filtered: List[List[Char]] = nums.filter(bit => bit(index) == common)
+      findBitString(filtered, index + 1)(commonOf)
+
     }
-
-    val bitString = getBitString(list).map(charMap)
-    _filterBasedOnCharIndex(list, 0, bitString, charMap)
 
   }
 
-  val inputs: List[String] = Source.fromResource("Day03.txt").getLines.toList
+  val inputs: List[List[Char]] = Source.fromResource("Day03.txt").getLines().toList.map(_.toList)
 
-  val gamma = getBitString(inputs)
+  val gamma = inputs.transpose.map(mostCommon).mkString("")
+  val epsilon = inputs.transpose.map(leastCommon).mkString("")
 
-  val epsilon = gamma.map(flipBits)
-
-  val generator = filterBasedOnCharIndex(inputs, 0)
-  val scrubber = filterBasedOnCharIndex(inputs, 0, flipBits)
+  val oxygen = findBitString(inputs)(mostCommon)
+  val co2 = findBitString(inputs)(leastCommon)
 
   println(s"Result 1: ${Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)}")
-  println(s"Result 2: ${Integer.parseInt(generator, 2) * Integer.parseInt(scrubber, 2)}")
+  println(s"Result 2: ${oxygen * co2}")
 
 }
--- src/test/scala/databrewers/adventofcode/Day03Spec.scala
+++ src/test/scala/databrewers/adventofcode/Day03Spec.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.should.Matchers
 
 class Day03Spec extends AnyFlatSpec with Matchers {
 
-  it should "get the gamma" in {
+  it should "get the gamma and epsilon" in {
 
     val input = List(
       "00100",
@@ -23,14 +23,15 @@ class Day03Spec extends AnyFlatSpec with Matchers {
       "01010"
     )
 
-    val expected = "10110"
+    val expected = ("10110", "01001")
 
-    val output = Day03.getBitString(input)
+    val output = Day03.getGammaEpsilon(input)
 
     output shouldBe expected
+
   }
 
-  it should "get the epsilon" in {
+  it should "get the oxygen and co2" in {
 
     val input = List(
       "00100",
@@ -47,56 +48,11 @@ class Day03Spec extends AnyFlatSpec with Matchers {
       "01010"
     )
 
-    val expected = "01001"
+    val expected = ("10111", "01010")
 
-    val output = Day03.getBitString(input).map(Day03.flipBits)
-
-    output shouldBe expected
-  }
-
-  it should "get the generator" in {
-    val input = List(
-      "00100",
-      "11110",
-      "10110",
-      "10111",
-      "10101",
-      "01111",
-      "00111",
-      "11100",
-      "10000",
-      "11001",
-      "00010",
-      "01010"
-    )
-
-    val expected = "10111"
-
-    val output = Day03.filterBasedOnCharIndex(input, 0)
+    val output = Day03.getOxygenCo2(input)
 
     output shouldBe expected
-  }
 
-  it should "get the scrubber" in {
-    val input = List(
-      "00100",
-      "11110",
-      "10110",
-      "10111",
-      "10101",
-      "01111",
-      "00111",
-      "11100",
-      "10000",
-      "11001",
-      "00010",
-      "01010"
-    )
-
-    val expected = "01010"
-
-    val output = Day03.filterBasedOnCharIndex(input, 0, Day03.flipBits)
-
-    output shouldBe expected
   }
 }
