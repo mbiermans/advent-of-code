3821f90d4c9ae5735023a620f779d8799d2c5dd0
Matthias Biermans <matthias.biermans@databrewers.io>
2021-12-16 14:07:35 +0100

    Add solution for part 2 day 10

--- src/main/scala/databrewers/adventofcode/Day10.scala
+++ src/main/scala/databrewers/adventofcode/Day10.scala
@@ -20,36 +20,53 @@ object Day10 extends App {
   def getPoints(char: Char): Int = char match {
     case ')' => 3
     case ']' => 57
-    case '>' => 1197
-    case '}' => 25137
-  } 
-
-
-  def matchBrackets(todo: List[Char], openBrackets: List[Char] = List.empty): Option[Char] = {
-    if(todo.isEmpty) None
-    else {
-      todo match {
-        case head :: tail if Set('(', '[', '<', '{').contains(head) =>
-          matchBrackets(tail, head :: openBrackets) 
-        case head :: tail => 
-          val closingBracket = getMatchingOpenBracket(head)
-          openBrackets match {
-            case h :: t if h == closingBracket => 
-              matchBrackets(tail, t)
-            case _ => Some(head)
-          }
-      }
-    }
-
+    case '}' => 1197
+    case '>' => 25137
   }
 
-  val result1 = for {
-    line    <- lines
-    bracket <- matchBrackets(line.toList)
-    points  =  getPoints(bracket)
-  } yield points
+  def matchBrackets(todo: List[Char], openBrackets: List[Char] = List.empty): Option[Char] = {
+    (todo, openBrackets) match {
+      case (Nil, Nil)                                                    => None
+      case (Nil, _)                                                      => None
+      case ((head :: tail), _) if Set('(', '[', '{', '<').contains(head) => matchBrackets(tail, head :: openBrackets)
+      case ((head :: tail), _)                                           =>
+        val matchingBracket = getMatchingOpenBracket(head)
+        openBrackets match {
+          case (headOpen :: tailOpen) if headOpen == matchingBracket => matchBrackets(tail, tailOpen)
+          case _                                                     => Some(head)
+        }
+    }
+  }
 
-  println(result1.sum)
+  def autoComplete(todo: List[Char], openBrackets: List[Char] = List.empty): List[Char] = {
+    (todo, openBrackets) match {
+      case (Nil, _)                                                    => openBrackets
+      case (head :: tail, _) if Set('(', '[', '{', '<').contains(head) => autoComplete(tail, head :: openBrackets)
+      case (head :: tail, Nil)                                         => autoComplete(tail, Nil)
+      case (head :: tail, headOpen :: tailOpen)                        => autoComplete(tail, tailOpen)
+    }
+  }
 
+  def autoCompletePoints(char: Char): Long = char match {
+    case '(' => 1L
+    case '[' => 2L
+    case '{' => 3L
+    case '<' => 4L
+  }
+
+  val results = for {
+    line      <- lines
+    bracket    = matchBrackets(line.toList)
+    points     = bracket.map(getPoints)
+    auto       = if (bracket.isEmpty) autoComplete(line.toList) else List.empty
+    autoPoints = auto.map(autoCompletePoints).foldLeft(0L){case (accum, score) => accum * 5L + score}
+  } yield (points, autoPoints)
+
+  val result1 = results.flatMap(_._1).sum
+  println(result1)
+
+  val result2List = results.filter(_._1.isEmpty).map(_._2).sorted
+  val result2 = result2List(result2List.size / 2)
+  println(result2)
 
 }
