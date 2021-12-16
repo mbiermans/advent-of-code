79440341b35025b8634a4800fb87d3aa9c5d742d
Matthias Biermans <matthias.biermans@databrewers.io>
2021-12-16 07:58:49 +0100

    Add solver for sample input...

--- src/main/scala/databrewers/adventofcode/Day10.scala
+++ src/main/scala/databrewers/adventofcode/Day10.scala
@@ -10,13 +10,46 @@ object Day10 extends App {
     .getLines()
     .toList
 
-  lines foreach println
-
-  val squareBracketRegex = "\\[([^\\[\\]+])".r
-
-  "[({(<(())[]>[[{[]{<()<>>" match {
-      case squareBracketRegex(a) => println("lol!")
+  def getMatchingOpenBracket(char: Char) = char match {
+    case ')' => '('
+    case ']' => '['
+    case '>' => '<'
+    case '}' => '{'
   }
+
+  def getPoints(char: Char): Int = char match {
+    case ')' => 3
+    case ']' => 57
+    case '>' => 1197
+    case '}' => 25137
+  } 
+
+
+  def matchBrackets(todo: List[Char], openBrackets: List[Char] = List.empty): Option[Char] = {
+    if(todo.isEmpty) None
+    else {
+      todo match {
+        case head :: tail if Set('(', '[', '<', '{').contains(head) =>
+          matchBrackets(tail, head :: openBrackets) 
+        case head :: tail => 
+          val closingBracket = getMatchingOpenBracket(head)
+          openBrackets match {
+            case h :: t if h == closingBracket => 
+              matchBrackets(tail, t)
+            case _ => Some(head)
+          }
+      }
+    }
+
+  }
+
+  val result1 = for {
+    line    <- lines
+    bracket <- matchBrackets(line.toList)
+    points  =  getPoints(bracket)
+  } yield points
+
+  println(result1.sum)
 
 
 }
